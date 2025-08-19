9391852a06f162c3e418c7a37a802877dcc241c9
Özgür Deli <oezguer.deli@gmail.com>
2025-08-19 20:08:54 +0200

    Fixed Eslint issue caused by missing spaces after comment symbol

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 import { argv } from 'node:process';
-import * as emoji from 'node-emoji';
+import emoji from 'node-emoji';
 
 if (argv.length < 3) {
-  //This happens only if there is no user input
+  // This happens only if there is no user input
   console.log(emoji.random().emoji);
+} else if (emoji.get(argv[2])) {
+  // This happens only if the emoji exists
+  console.log(emoji.get(argv[2]));
 } else {
-  if (emoji.has(':' + argv[2] + ':')) {
-    //This happens only if the emoji exists
-    console.log(emoji.get(argv[2]));
-  } else {
-    //This happens if the user input is undefined
-    console.log(`This emoji ${argv[2]} does not exist`);
-  }
+  // This happens if the user input is undefined
+  console.log(`This emoji ${argv[2]} does not exist`);
 }
